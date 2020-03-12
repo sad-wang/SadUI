df262a90dbfb2f2947acb43cf10f45988198c973
sad-wang <leown_wang@163.com>
2020-03-13 02:45:12 +0800

    completed sad-tabs component basically

--- src/App.vue
+++ src/App.vue
@@ -28,15 +28,46 @@
         <sad-input  v-model="value" :value="value"/> value: {{value}}
       </div>
     </div>
+    <div>
+      <sad-tabs selected="finance" >
+        <sad-tabs-head >
+          <sad-tabs-item name="disabled" disabled >
+            不可用
+          </sad-tabs-item>
+          <sad-tabs-item name="finance">
+            财经
+          </sad-tabs-item>
+          <sad-tabs-item name="sports">
+            体育
+          </sad-tabs-item>
+        </sad-tabs-head>
+        <sad-tabs-body>
+          <sad-tabs-panel name="disabled">
+            相关资讯
+          </sad-tabs-panel>
+          <sad-tabs-panel name="finance">
+            财经相关资讯
+          </sad-tabs-panel>
+          <sad-tabs-panel name="sports">
+            体育相关资讯
+          </sad-tabs-panel>
+        </sad-tabs-body>
+      </sad-tabs>
+    </div>
   </div>
 </template>
 
 <script>
 import SadButton from './components/sad-button'
 import SadInput from './components/sad-input'
+import sadTabs from './components/sad-tabs'
+import sadTabsBody from './components/sad-tabs-body'
+import sadTabsHead from './components/sad-tabs-head'
+import sadTabsItem from './components/sad-tabs-item'
+import sadTabsPanel from './components/sad-tabs-panel'
 export default {
   name: 'app',
-  components: {SadButton, SadInput},
+  components: {SadButton, SadInput, sadTabs, sadTabsBody, sadTabsHead, sadTabsPanel ,sadTabsItem},
   data:()=>{
     return{
       value: '支持双向绑定'
---
+++ src/components/sad-tabs-body.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="tabs-body">
+        <slot></slot>
+    </div></template>
+<script>
+    export default {
+        name: "sad-tabs-body",
+        inject: ['eventBus'],
+        created(){
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
